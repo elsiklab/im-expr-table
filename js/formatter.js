526e827e67eb714e91b546c9504f081767e939b3
Colin <colin.diesh@gmail.com>
2015-09-09 05:26:15 -0500

    Open new tab/window

--- js/formatter.js
+++ js/formatter.js
@@ -7,8 +7,8 @@ module.exports = simpleFormatter(
         var s=val.sampleName;
         var tracks1=s+","+s+"_XYPlot,"+s+"_Density"+","+s+"_cufflinks_chr_2015_09_02_transcripts"+","+s+"_tophat_chr_2015_09_02_junctions";
         var tracks2=s+","+s+"_XYPlot,"+s+"_Density"+","+s+"_Cufflinks"+","+s+"_0_2013_12_11__junctions";
-        var link1="<a href=http://bovinegenome.org:8080/Btau_4.6.1/jbrowse/?loc="+$("#transcript_id").val()+"&tracks="+tracks1+">Btau_4.6.1</a>";
-        var link2="<a href=http://bovinegenome.org:8080/UMD3.1/jbrowse/?loc="+$("#transcript_id").val()+"&tracks="+tracks2+">UMD3.1</a>";
+        var link1="<a target=\"_blank\" href=http://bovinegenome.org:8080/Btau_4.6.1/jbrowse/?loc="+$("#transcript_id").val()+"&tracks="+tracks1+">Btau_4.6.1</a>";
+        var link2="<a target=\"_blank\" href=http://bovinegenome.org:8080/UMD3.1/jbrowse/?loc="+$("#transcript_id").val()+"&tracks="+tracks2+">UMD3.1</a>";
         return s+ " ["+link1+"] [" + link2 + "]";
     }
 );
