094bc66ad1f2385c937a658679bcb56497ba9e4b
Colin <colin.diesh@gmail.com>
2015-09-09 04:35:18 -0500

    Fix track linker

--- js/formatter.js
+++ js/formatter.js
@@ -6,6 +6,6 @@ module.exports = simpleFormatter(
     function (val) {
         var s=val.sampleName;
         var tracks=s+","+s+"_XYPlot,"+s+"_Density"+","+s+"_cufflinks_chr_2015_09_02_transcripts"+","+s+"_tophat_chr_2015_09_02_junctions";
-        return "<a href=http://bovinegenome.org:8080/Btau_4.6.1/jbrowse/?loc="+$("transcript_id").val()+"&tracks="+tracks+">JBrowse</a>";
+        return "<a href=http://bovinegenome.org:8080/Btau_4.6.1/jbrowse/?loc="+$("#transcript_id").val()+"&tracks="+tracks+">JBrowse</a>";
     }
 );
