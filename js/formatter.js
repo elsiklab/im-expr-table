2c21821e515a34ca9d6ea73bed7818c21e2da437
Colin <colin.diesh@gmail.com>
2015-09-09 04:32:47 -0500

    Update formatter tracks

--- js/formatter.js
+++ js/formatter.js
@@ -1,11 +1,11 @@
-var simpleFormatter = require('non-escaping-simple-formatter');
+var simpleFormatter = require('./non-escaping-simple-formatter');
 var _ = require('underscore');
 module.exports = simpleFormatter(
     'Expression', // The type of thing this formatter handles
     ['sampleName'], // The fields it formats
     function (val) {
-        //val['report:uri']="http://google.com";
-        //val['service:base']="http://google.com";
-        return "TEST<br/>TEST";
+        var s=val.sampleName;
+        var tracks=s+","+s+"_XYPlot,"+s+"_Density"+","+s+"_cufflinks_chr_2015_09_02_transcripts"+","+s+"_tophat_chr_2015_09_02_junctions";
+        return "<a href=http://bovinegenome.org:8080/Btau_4.6.1/jbrowse/?loc="+$("transcript_id").val()+"&tracks="+tracks+">JBrowse</a>";
     }
 );
